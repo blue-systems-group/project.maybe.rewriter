02ddac290409e30c064a677824c12c59c26b29ba
jerry <jerryant@buffalo.edu>
2015-03-02 12:44:15 -0500

    Bug fixes and removal of redundant code.

--- find-if-else.py
+++ find-if-else.py
@@ -2,7 +2,7 @@
 import sys, os, io, re
 import argparse, json
 
-def remove_comments_and_strings(content):
+def remove_comments(content):
   MULTI_LINE_COMMENT_PATTERN = re.compile(r"""(?s)/\*.*?\*/""")
   SINGLE_LINE_COMMENT_PATTERN = re.compile(r"""(?m)//.*$""")
 
@@ -11,41 +11,17 @@ def remove_comments_and_strings(content):
   
   return content
 
-class MaybeAlternative(object):
-  def __init__(self, value, offset, start, end, content):
-    self.value = value
-    self.offset = offset
-    self.start = start - offset
-    self.end = end - offset
-    self.content = content
-
-
-class MaybeStatement(object):
-  BLOCK = "block"
-
-  def __init__(self, maybe_type, start, end=None):
-    self.maybe_type = maybe_type
-    self.start = start
-    self.end = end
-    self.alternatives = []
-    self.content = None
-    self.line = None
-
-  def __repr__(self):
-    return "{{{maybe_type} {start}:{end}}}".format(maybe_type=self.maybe_type,
-                                                start=self.start, end=self.end)
-
-
 
 def is_block(string):
   return (string.count("{") - string.count("}") == 0)
 
 
-ALTERNATIVE_START_PATTERN = re.compile(r"""^\s*else\s*{""")
+ALTERNATIVE_START_PATTERN_ONE = re.compile(r"""^\s*else\s*{""")
+ALTERNATIVE_START_PATTERN_TWO = re.compile(r"""^\s*else\sif\s*{""")
 
 def match_to_block(match, content):
-  maybe_block = MaybeStatement(MaybeStatement.BLOCK, match.start())
-  maybe_block.line = len(content[:match.start()].splitlines()) + 1
+  #print match.group(0)[:-1]
+  string = match.group(0)[:-1]
   buffer_start = match.end() - 1
 
   value = 0
@@ -59,26 +35,25 @@ def match_to_block(match, content):
       buffer_end += buffer_increment + 1
       block_buffer = content[buffer_start:buffer_end]
       if is_block(block_buffer):
-        maybe_block.alternatives.append(MaybeAlternative(value,
-                                          maybe_block.start,
-                                          buffer_start + 1,
-                                          buffer_end,
-                                          block_buffer[1:-1]))
         value += 1
         break
       else:
         search_start += buffer_increment + 1
-    else_match = ALTERNATIVE_START_PATTERN.match(content[buffer_end:])
+    else_match = ALTERNATIVE_START_PATTERN_ONE.match(content[buffer_end:])
     if else_match != None:
       buffer_start = buffer_end + else_match.end() - 1
     else:
-      break
-  maybe_block.end = buffer_end
-  maybe_block.content = content[maybe_block.start:maybe_block.end]
-  return maybe_block.content
+      else_match = ALTERNATIVE_START_PATTERN_TWO.match(content[buffer_end:])
+      if else_match != None:
+        buffer_start = buffer_end + else_match.end() - 1
+      else:
+        break
+#  print content[match.start():buffer_end]
+#  print "==================="
+  return content[match.start():buffer_end]
 
 
-BLOCK_START_PATTERN = re.compile(r"""^(?m)(?P<indent>[^\S\n]*)if\s*\((?P<label>.+?)\)\s*{""")
+BLOCK_START_PATTERN = re.compile(r"""^(?m)(?P<indent>[^\S\n]*)if\s*\((?P<condition>.+?)\)\s*{""")
 
 
 def record_blocks(content):
@@ -86,6 +61,8 @@ def record_blocks(content):
   for match in BLOCK_START_PATTERN.finditer(content):
     #print match.group(0)[:-1]
     if_else_block = match_to_block(match, content)
+#    print if_else_block
+#    print "============"
     statements.append(if_else_block)
   return statements
 
@@ -96,16 +73,19 @@ if __name__ == '__main__':
                                                 if-else blocks.")
   args = parser.parse_args()
   possible_entensions = ['.java']
+  dont_parse = ['ClipData.java', 'IntentResolver.java', \
+      'DocumentsActivity.java', 'VideoEditorActivity.java', \
+      'CameraActivity.java', 'DriverRS.java.template']
   
   for root, dirs, files in os.walk(args.source):
     for file in files:
       basename, ext = os.path.splitext(file)
-      if ext in possible_entensions:
+      if (ext in possible_entensions) and (file not in dont_parse):
         data_to_print = {}
         data_to_print[root+"/"+file] = []
         f = open(root+"/"+file, "r")
         file_content = f.read()
-        inter_file_content = remove_comments_and_strings(file_content)
+        inter_file_content = remove_comments(file_content)
         blocks = record_blocks(inter_file_content)
         for block in blocks:
           data_to_print[root+"/"+file].append(block)
